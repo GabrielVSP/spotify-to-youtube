56cbb2886a0ff3544c10fde445adb0a255eee80d
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-12-20 18:32:32 -0300

    Update: get algumbid now returns: artists, tracks and album anme

--- app/api/album/[albumId]/route.ts
+++ app/api/album/[albumId]/route.ts
@@ -4,6 +4,10 @@ import { NextResponse } from "next/server"
 
 const tokenKey = process.env.TOKEN_KEY
 
+interface Track {
+    name: string
+}
+
 export async function GET ( req: Request, { params }: { params: { albumId: string}}) {
  
     try {
@@ -26,19 +30,35 @@ export async function GET ( req: Request, { params }: { params: { albumId: strin
 
         }
 
-        let res
-
         try {
-            res = await axios.get(`https://api.spotify.com/v1/albums/${params.albumId}`, {
+
+            const res = await axios.get(`https://api.spotify.com/v1/albums/${params.albumId}`, {
                 headers: {
                     'Authorization': `Bearer ${token.value}`
                 }
             })
+
+
+            const trackNames: string[] = []
+            const artist: string = ''
+
+            const json = res.data.tracks.items.map((track: Track) => {
+
+                trackNames.push(track.name)
+
+            })
+
+            return NextResponse.json({
+                albumName: res.data.name,
+                songsName: trackNames,
+                artist: res.data.artists[0].name
+            })
+
         } catch(e: any) {
             return new NextResponse("Erro na requisição da API, verifique o ID e tente novamente", { status: e.status})
         }
 
-        return NextResponse.json(res.data)
+       
 
     } catch(e: any) {
 
