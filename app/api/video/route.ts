45b401a9884ab5d57ba47dd0c138e615c0f85e28
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-12-21 14:18:52 -0300

    Add: web scrapping to get videos urls

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,35 +1,49 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
 
 export async function GET( req: Request ) {
     
     try {
 
-        
-
         const url = new URL(req.url)
         const dataQuery: string = url.searchParams.get('data') || ''
 
+        if(!dataQuery) return new NextResponse('Nenhum dado foi identificado na query', { status: 400})
+
+        const browser = await puppeteer.launch()
+        
         const jsonString = Buffer.from(dataQuery, 'base64').toString('utf-8')
         const albumData = JSON.parse(jsonString)
 
         const ids: any[] = []
-        let test
-
-        for (const song of albumData.data.songsName) {
-            const query = `${albumData.data.artist} ${song}`;
-            const response = await axios.get(
-              `https://www.googleapis.com/youtube/v3/search?key=${process.env.YT_API_KEY}&q=${query}&type=video&part=snippet`
-            );
-            ids.push(response.data);
-          }
         
-        // albumData.data.songsName.forEach( async (song: any) => {
+        for( const song of albumData.data.songsName) {
+
+            const page = await browser.newPage()
             
-        //     axios.get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.YT_API_KEY}&q=${albumData.data.artist} ${song}&type=video&part=snippet`)
-        //     .then((res) =>  ids.push(res.data))
+            const searchQuery = `${albumData.data.artist} ${song}`;
+            const searchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchQuery)}`;
+      
+            // Navega até a página de resultados
+            await page.goto(searchUrl, { waitUntil: 'domcontentloaded' });
+            await page.waitForSelector('ytd-video-renderer');
+      
+            // Coleta a URL do vídeo com maior relevância
+            const videoUrl = await page.evaluate(() => {
+              const videoElement = document.querySelector('ytd-video-renderer a#video-title');
+              return videoElement ? (videoElement as HTMLAnchorElement).href : null;
+            });
+
+          if(!videoUrl) return new NextResponse("Nenhum vídeo encontrado")
            
-        // })
+          ids.push(videoUrl)
+
+          page.close()
+
+        }
+
+        await browser.close()
 
         return NextResponse.json(ids)
 
